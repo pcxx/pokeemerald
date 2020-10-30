b737b2c3af3b4318cba2d8e5e0129c9c4bd99b57
Henny022p <info@henny022.de>
2020-10-30 23:02:39 +0100

    replaced dma macros with constexpr fuctions

--- include/gba/macro.h
+++ include/gba/macro.h
@@ -1,6 +1,10 @@
 #ifndef GUARD_GBA_MACRO_H
 #define GUARD_GBA_MACRO_H
 
+#include <type_traits>
+
+#include "types.h"
+
 #define CPU_FILL(value, dest, size, bit)                                          \
 {                                                                                 \
     vu##bit tmp = (vu##bit)(value);                                               \
@@ -31,155 +35,107 @@
 
 #define CpuFastCopy(src, dest, size) CpuFastSet(src, dest, ((size)/(32/8) & 0x1FFFFF))
 
-#define DmaSet(dmaNum, src, dest, control)        \
-{                                                 \
-    vu32 *dmaRegs = (vu32 *)REG_ADDR_DMA##dmaNum; \
-    dmaRegs[0] = (vu32)(src);                     \
-    dmaRegs[1] = (vu32)(dest);                    \
-    dmaRegs[2] = (vu32)(control);                 \
-    dmaRegs[2];                                   \
+template<typename T>
+concept is_u16 = std::is_same<T, vu16>::value;
+
+template<typename T>
+concept is_u32 = std::is_same<T, vu32>::value;
+
+template<u32 dmaNum>
+concept is_valid_dma_num = dmaNum < 4;
+
+constexpr u32 REG_ADDR_DMAx[] = {REG_ADDR_DMA0, REG_ADDR_DMA1, REG_ADDR_DMA2, REG_ADDR_DMA3};
+
+template<u32 dmaNum, typename T>
+constexpr void DmaSet(T *src, T *dest, u32 control) requires (is_u16<T> || is_u32<T>) && is_valid_dma_num<dmaNum>
+{
+    vu32 *dmaRegs = reinterpret_cast<vu32 *>(REG_ADDR_DMAx[dmaNum]);
+    dmaRegs[0] = reinterpret_cast<u32>(src);
+    dmaRegs[1] = reinterpret_cast<u32>(dest);
+    dmaRegs[2] = control;     
+    dmaRegs[2]; // I guess you have to read from the register to ? start dma? wait for it to finish?
 }
 
-#define DMA_FILL(dmaNum, value, dest, size, bit)                                              \
-{                                                                                             \
-    vu##bit tmp = (vu##bit)(value);                                                           \
-    DmaSet(dmaNum,                                                                            \
-           &tmp,                                                                              \
-           dest,                                                                              \
-           (DMA_ENABLE | DMA_START_NOW | DMA_##bit##BIT | DMA_SRC_FIXED | DMA_DEST_INC) << 16 \
-         | ((size)/(bit/8)));                                                                 \
+template<u32 dmaNum, typename T>
+constexpr void DmaFill(u32 value, T *dest, u32 size) requires is_u16<T>
+{
+    volatile T tmp = value;
+    u32 control = (DMA_ENABLE | DMA_START_NOW | DMA_16BIT | DMA_SRC_FIXED | DMA_DEST_INC) << 16 | size;
+    DmaSet<dmaNum>(&tmp, dest, control);
 }
 
-#define DmaFill16(dmaNum, value, dest, size) DMA_FILL(dmaNum, value, dest, size, 16)
-#define DmaFill32(dmaNum, value, dest, size) DMA_FILL(dmaNum, value, dest, size, 32)
+template<u32 dmaNum, typename T>
+constexpr void DmaFill(u32 value, T *dest, u32 size) requires is_u32<T>
+{
+    volatile T tmp = value;
+    u32 control = (DMA_ENABLE | DMA_START_NOW | DMA_32BIT | DMA_SRC_FIXED | DMA_DEST_INC) << 16 | size;
+    DmaSet<dmaNum>(&tmp, dest, control);
+}
 
 // Note that the DMA clear macros cause the DMA control value to be calculated
 // at runtime rather than compile time. The size is divided by the DMA transfer
 // unit size (2 or 4 bytes) and then combined with the DMA control flags using a
 // bitwise OR operation.
 
-#define DMA_CLEAR(dmaNum, dest, size, bit)  \
-{                                           \
-    vu##bit *_dest = (vu##bit *)(dest);     \
-    u32 _size = size;                       \
-    DmaFill##bit(dmaNum, 0, _dest, _size);  \
+template<u32 dmaNum, typename T>
+constexpr void DmaClear(T *dest, u32 size)
+{
+    DmaFill<dmaNum>(0, dest, size);
 }
 
-#define DmaClear16(dmaNum, dest, size) DMA_CLEAR(dmaNum, dest, size, 16)
-#define DmaClear32(dmaNum, dest, size) DMA_CLEAR(dmaNum, dest, size, 32)
-
-#define DMA_COPY(dmaNum, src, dest, size, bit)                                              \
-    DmaSet(dmaNum,                                                                          \
-           src,                                                                             \
-           dest,                                                                            \
-           (DMA_ENABLE | DMA_START_NOW | DMA_##bit##BIT | DMA_SRC_INC | DMA_DEST_INC) << 16 \
-         | ((size)/(bit/8)))
-
-#define DmaCopy16(dmaNum, src, dest, size) DMA_COPY(dmaNum, src, dest, size, 16)
-#define DmaCopy32(dmaNum, src, dest, size) DMA_COPY(dmaNum, src, dest, size, 32)
-
-#define DmaCopyLarge(dmaNum, src, dest, size, block, bit) \
-{                                                         \
-    const void *_src = src;                               \
-    void *_dest = dest;                                   \
-    u32 _size = size;                                     \
-    while (1)                                             \
-    {                                                     \
-        DmaCopy##bit(dmaNum, _src, _dest, (block));       \
-        _src += (block);                                  \
-        _dest += (block);                                 \
-        _size -= (block);                                 \
-        if (_size <= (block))                             \
-        {                                                 \
-            DmaCopy##bit(dmaNum, _src, _dest, _size);     \
-            break;                                        \
-        }                                                 \
-    }                                                     \
+template<u32 dmaNum, typename T>
+constexpr void DmaCopy(T *src, T *dest, u32 size) requires is_u16<T>
+{
+    u32 control = (DMA_ENABLE | DMA_START_NOW | DMA_16BIT | DMA_SRC_INC | DMA_DEST_INC) << 16 | size;
+    DmaSet<dmaNum>(src, dest, control);
 }
 
-#define DmaCopyLarge16(dmaNum, src, dest, size, block) DmaCopyLarge(dmaNum, src, dest, size, block, 16)
-
-#define DmaCopyLarge32(dmaNum, src, dest, size, block) DmaCopyLarge(dmaNum, src, dest, size, block, 32)
-
-#define DmaFillLarge(dmaNum, value, dest, size, block, bit) \
-{                                                           \
-    void *_dest = dest;                                     \
-    u32 _size = size;                                       \
-    while (1)                                               \
-    {                                                       \
-        DmaFill##bit(dmaNum, value, _dest, (block));       \
-        _dest += (block);                                   \
-        _size -= (block);                                   \
-        if (_size <= (block))                               \
-        {                                                   \
-            DmaFill##bit(dmaNum, value, _dest, _size);     \
-            break;                                          \
-        }                                                   \
-    }                                                       \
+template<u32 dmaNum, typename T>
+constexpr void DmaCopy(T *src, T *dest, u32 size) requires is_u32<T>
+{
+    u32 control = (DMA_ENABLE | DMA_START_NOW | DMA_32BIT | DMA_SRC_INC | DMA_DEST_INC) << 16 | size;
+    DmaSet<dmaNum>(src, dest, control);
 }
 
-#define DmaFillLarge16(dmaNum, value, dest, size, block) DmaFillLarge(dmaNum, value, dest, size, block, 16)
-
-#define DmaFillLarge32(dmaNum, value, dest, size, block) DmaFillLarge(dmaNum, value, dest, size, block, 32)
-
-#define DmaClearLarge(dmaNum, dest, size, block, bit) \
-{                                                           \
-    void *_dest = dest;                                     \
-    u32 _size = size;                                       \
-    while (1)                                               \
-    {                                                       \
-        DmaFill##bit(dmaNum, 0, _dest, (block));       \
-        _dest += (block);                                   \
-        _size -= (block);                                   \
-        if (_size <= (block))                               \
-        {                                                   \
-            DmaFill##bit(dmaNum, 0, _dest, _size);     \
-            break;                                          \
-        }                                                   \
-    }                                                       \
+template<u32 dmaNum, typename T>
+constexpr void DmaCopyLarge(T *src, T *dest, u32 size, u32 block_size)
+{
+    while (size >= block_size)
+    {
+        DmaCopy<dmaNum>(src, dest, block_size);
+        src += block_size;
+        dest += block_size;
+        size -= block_size;
+    }
+    DmaCopy<dmaNum>(src, dest, size);
+    
 }
 
-#define DmaClearLarge16(dmaNum, dest, size, block) DmaClearLarge(dmaNum, dest, size, block, 16)
-
-#define DmaClearLarge32(dmaNum, dest, size, block) DmaClearLarge(dmaNum, dest, size, block, 32)
-
-#define DmaCopyDefvars(dmaNum, src, dest, size, bit) \
-{                                                    \
-    const void *_src = src;                          \
-    void *_dest = dest;                              \
-    u32 _size = size;                                \
-    DmaCopy##bit(dmaNum, _src, _dest, _size);        \
+template<u32 dmaNum, typename T>
+constexpr void DmaFillLarge(u32 value, T *dest, u32 size, u32 block_size)
+{
+    while(size >= block_size)
+    {
+        DmaFill<dmaNum>(value, dest, block_size);
+        dest += block_size;
+        size -= block_size;
+    }
+    DmaFill<dmaNum>(value, dest, size);
 }
 
-#define DmaCopy16Defvars(dmaNum, src, dest, size) DmaCopyDefvars(dmaNum, src, dest, size, 16)
-#define DmaCopy32Defvars(dmaNum, src, dest, size) DmaCopyDefvars(dmaNum, src, dest, size, 32)
-
-#define DmaFillDefvars(dmaNum, value, dest, size, bit) \
-{                                                      \
-    void *_dest = dest;                                \
-    u32 _size = size;                                  \
-    DmaFill##bit(dmaNum, value, _dest, _size);         \
+template<u32 dmaNum, typename T>
+constexpr void DmaClearLarge(T *dest, u32 size, u32 block_size)
+{
+    DmaFillLarge<dmaNum>(0, dest, size, block_size);
 }
 
-#define DmaFill16Defvars(dmaNum, value, dest, size) DmaFillDefvars(dmaNum, value, dest, size, 16)
-#define DmaFill32Defvars(dmaNum, value, dest, size) DmaFillDefvars(dmaNum, value, dest, size, 32)
-
-#define DmaClearDefvars(dmaNum, dest, size, bit) \
-{                                                \
-    void *_dest = dest;                          \
-    u32 _size = size;                            \
-    DmaClear##bit(dmaNum, _dest, _size);         \
-}
-
-#define DmaClear16Defvars(dmaNum, dest, size) DmaClearDefvars(dmaNum, dest, size, 16)
-#define DmaClear32Defvars(dmaNum, dest, size) DmaClearDefvars(dmaNum, dest, size, 32)
-
-#define DmaStop(dmaNum)                                         \
-{                                                               \
-    vu16 *dmaRegs = (vu16 *)REG_ADDR_DMA##dmaNum;               \
-    dmaRegs[5] &= ~(DMA_START_MASK | DMA_DREQ_ON | DMA_REPEAT); \
-    dmaRegs[5] &= ~DMA_ENABLE;                                  \
-    dmaRegs[5];                                                 \
+template<u32 dmaNum>
+constexpr void DmaStop() requires is_valid_dma_num<dmaNum>
+{
+    vu16 *dmaRegs = reinterpret_cast<vu16 *>(REG_ADDR_DMAx[dmaNum]);
+    dmaRegs[5] &= ~(DMA_START_MASK | DMA_DREQ_ON | DMA_REPEAT);
+    dmaRegs[5] &= ~DMA_ENABLE;
+    dmaRegs[5];
 }
 
 #define IntrEnable(flags)                                       \
