5525e78488a08d04cb8ef2dde79df9998a10d28d
Henny022p <info@henny022.de>
2020-10-30 23:44:48 +0100

    swaped defines for constexpr stuff

--- include/gba/defines.h
+++ include/gba/defines.h
@@ -3,6 +3,8 @@
 
 #include <cstddef>
 
+#include "types.h"
+
 #define TRUE  1
 #define FALSE 0
 
@@ -23,55 +25,74 @@
 #define INTR_CHECK     (*(u16 *)0x3007FF8)
 #define INTR_VECTOR    (*(void **)0x3007FFC)
 
-#define EWRAM_START 0x02000000
-#define EWRAM_END   (EWRAM_START + 0x40000)
-#define IWRAM_START 0x03000000
-#define IWRAM_END   (IWRAM_START + 0x8000)
+constexpr u32 EWRAM_START = 0x02000000;
+constexpr u32 EWRAM_END   = EWRAM_START + 0x40000;
+constexpr u32 IWRAM_START = 0x03000000;
+constexpr u32 IWRAM_END   = IWRAM_START + 0x8000;
 
-#define PLTT      0x5000000
-#define PLTT_SIZE 0x400
+constexpr u32 PLTT      = 0x5000000;
+constexpr u32 PLTT_SIZE = 0x400;
 
-#define BG_PLTT      PLTT
-#define BG_PLTT_SIZE 0x200
+constexpr u32 BG_PLTT      = PLTT;
+constexpr u32 BG_PLTT_SIZE = 0x200;
 
-#define OBJ_PLTT      (PLTT + 0x200)
-#define OBJ_PLTT_SIZE 0x200
+constexpr u32 OBJ_PLTT      = PLTT + 0x200;
+constexpr u32 OBJ_PLTT_SIZE = 0x200;
 
-#define VRAM      0x6000000
-#define VRAM_SIZE 0x18000
+constexpr u32 VRAM      = 0x6000000;
+constexpr u32 VRAM_SIZE = 0x18000;
 
-#define BG_VRAM           VRAM
-#define BG_VRAM_SIZE      0x10000
-#define BG_CHAR_SIZE      0x4000
-#define BG_SCREEN_SIZE    0x800
-#define BG_CHAR_ADDR(n)   (BG_VRAM + (BG_CHAR_SIZE * (n)))
-#define BG_SCREEN_ADDR(n) (BG_VRAM + (BG_SCREEN_SIZE * (n)))
+constexpr u32 BG_VRAM           = VRAM;
+constexpr u32 BG_VRAM_SIZE      = 0x10000;
+constexpr u32 BG_CHAR_SIZE      = 0x4000;
+constexpr u32 BG_SCREEN_SIZE    = 0x800;
 
-#define BG_TILE_H_FLIP(n) (0x400 + (n))
-#define BG_TILE_V_FLIP(n) (0x800 + (n))
+constexpr u32 BG_CHAR_ADDR(u32 n)
+{
+    return BG_VRAM + (BG_CHAR_SIZE * n);
+}
+constexpr u32 BG_SCREEN_ADDR(u32 n)
+{
+    return BG_VRAM + (BG_SCREEN_SIZE * n);
+}
+
+constexpr u32 BG_TILE_H_FLIP(u32 n)
+{
+    return 0x400 + n;
+}
+constexpr u32 BG_TILE_V_FLIP(u32 n)
+{
+    return 0x800 + n;
+}
 
 // text-mode BG
-#define OBJ_VRAM0      (VRAM + 0x10000)
-#define OBJ_VRAM0_SIZE 0x8000
+constexpr u32 OBJ_VRAM0      = VRAM + 0x10000;
+constexpr u32 OBJ_VRAM0_SIZE = 0x8000;
 
 // bitmap-mode BG
-#define OBJ_VRAM1      (VRAM + 0x14000)
-#define OBJ_VRAM1_SIZE 0x4000
+constexpr u32 OBJ_VRAM1      = VRAM + 0x14000;
+constexpr u32 OBJ_VRAM1_SIZE = 0x4000;
 
-#define OAM      0x7000000
-#define OAM_SIZE 0x400
+constexpr u32 OAM      = 0x7000000;
+constexpr u32 OAM_SIZE = 0x400;
 
-#define ROM_HEADER_SIZE   0xC0
+constexpr u32 ROM_HEADER_SIZE = 0xC0;
 
-#define DISPLAY_WIDTH  240
-#define DISPLAY_HEIGHT 160
+constexpr u32 DISPLAY_WIDTH  = 240;
+constexpr u32 DISPLAY_HEIGHT = 160;
 
-#define TILE_SIZE_4BPP 32
-#define TILE_SIZE_8BPP 64
+constexpr u32 TILE_SIZE_4BPP = 32;
+constexpr u32 TILE_SIZE_8BPP = 64;
 
-#define TILE_OFFSET_4BPP(n) ((n) * TILE_SIZE_4BPP)
-#define TILE_OFFSET_8BPP(n) ((n) * TILE_SIZE_8BPP)
+constexpr u32 TILE_OFFSET_4BPP(u32 n)
+{
+    return n * TILE_SIZE_4BPP;
+}
+constexpr u32 TILE_OFFSET_8BPP(u32 n)
+{
+    return n * TILE_SIZE_8BPP;
+}
 
-#define TOTAL_OBJ_TILE_COUNT 1024
+constexpr u32 TOTAL_OBJ_TILE_COUNT = 1024;
 
 #endif // GUARD_GBA_DEFINES
